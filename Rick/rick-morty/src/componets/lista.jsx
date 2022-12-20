5d308f502568dd166f924f7810382fc81f34e472
Mp-keyner <keyneroswaldo0@gmail.com>
2022-12-20 12:44:01 -0500

    primero

---
+++ Rick/rick-morty/src/componets/lista.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react'
+import Personaje from './personaje';
+
+
+function Navpagina({pagina , setpagina }) {
+    return (
+        <header className='d-flex align-items-center'>
+            <p className='page'>Page : {pagina}</p>
+            <button className='btn btn-primary btn-sm pote'
+                onClick={() => {
+                    setpagina(pagina - 1) 
+                }}
+            >Page {pagina - 1}</button>
+            <button className='btn btn-primary btn-sm pote'
+                onClick={() => {
+                    setpagina(pagina + 1) 
+                }}
+            >Page {pagina + 1}</button>
+        </header>
+    )
+}
+
+
+const Lista = () => {
+
+    const [caracter, setcaracter] = useState([])
+    const [pagina, setpagina] = useState(1)
+    useEffect(() => {
+        async function fetchData() {
+            const data = await fetch(
+                `https://rickandmortyapi.com/api/character?page=${pagina}`
+            );
+            const dato = await data.json();
+            setcaracter(dato.results)
+        }
+        fetchData();
+    }, [pagina]);
+
+
+    return (
+        <div className='container'>
+            <Navpagina pagina={pagina} setpagina={setpagina}/>
+            <div className='row'>
+                {caracter.map(caracter => {
+                    return (
+                        <div className='col-md-6' key={caracter.id}>
+                            <Personaje caracter={caracter} />
+                        </div>
+                    )
+                })}
+            <Navpagina pagina={pagina} setpagina={setpagina}/>
+            </div>
+        </div>
+    );
+}
+
+export default Lista;
